a83272187bb8c14dbb94a82c1135587d5770b0d8
Ryan Tanay <ryantanay@newevolution.org>
2016-05-31 11:39:23 -0500

    Injected  for retrieving data from JSON, added grouping in a dropdown for tasks, added radio button functionality for selecting task type

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller("TodoCtrl", ($scope) => {
+app.controller("TodoCtrl", ($scope, $http) => {
 
   $scope.title = 'Angular Todo - PFM!';
   $scope.filter2 = '';
@@ -8,23 +8,46 @@ app.controller("TodoCtrl", ($scope) => {
   $scope.variableName = '';
 
   $scope.newTodo = '';
+  $scope.taskType = 'home';
 
-  $scope.tasks = [
-    { name: "Make the bed", type: "home"},
-    { name: "Eat breakfast", type: "home"},
-    { name: "Check the network", type: "work"},
-    { name: "Start up Slack", type: "work"},
-    { name: "Feed the cats", type: "home"}
-  ];
+  $scope.selectedTodo = 'nothing';
+
+  // $scope.tasks = [
+  //   { name: "Make the bed", type: "home"},
+  //   { name: "Eat breakfast", type: "home"},
+  //   { name: "Check the network", type: "work"},
+  //   { name: "Start up Slack", type: "work"},
+  //   { name: "Feed the cats", type: "home"}
+  // ];
+
+  // $http({
+  //   method: 'GET',
+  //   url: '/data/tasks.json'
+  // }).then((response) => {
+  //   $scope.tasks = response.data.tasks;
+  // })
+
+  $http.get('/data/tasks.json')
+    .then((response) => {
+      $scope.tasks = response.data.tasks;
+    })
+
+  // $http.post('/data/tasks.json', {data: "what I'm sending"})
+  //   .then((response) => {
+  //     console.log("response", response);
+  //     $scope.tasks = response.data.tasks;
+  // })
 
   $scope.addTodo = () => {
-    $scope.tasks.push({ name: $scope.newTodo, type: "home"});
+    $scope.tasks.push({ name: $scope.newTodo, type: $scope.taskType});
     $scope.newTodo = '';
   }
 
   $scope.removeTodo = (task) => {
     const taskIndex = $scope.tasks.indexOf(task);
-    $scope.tasks.splice(taskIndex, 1); // eslint-disable-line no-magic-numbers
+    if (taskIndex >= 0) { // eslint-disable-line no-magic-numbers
+      $scope.tasks.splice(taskIndex, 1); // eslint-disable-line no-magic-numbers
+    }
   }
 
 })
